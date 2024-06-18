8025f3562b473c30d21d86751a01348a0b075388
brunnovaz1 <brunnovaz@gmail.com>
2024-06-18 12:02:05 -0300

    inclusao middleware e mudanca rotas (close #5)

---
+++ api/middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+function validarToken(req, res, next) {
+    const token = req.headers["authorization"]
+    if(token) {
+        try{
+            const payload = jwt.verify(token, process.env.SEGREDO);
+            console.log(payload)
+            next()
+        } catch(err) {
+            res.status(400).json({msg: 'token errado!'})     
+        }
+    } else {
+    res.status(400).json({msg: 'token inválido!'})
+    }
+}
--- api/routes/router_artista.js
+++ api/routes/router_artista.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const validarToken = require("../middlewares/auth")
 const controllerArtista = require('../controllers/controller_artista');
 
 const router = express.Router();
 
 
-router.post('/', controllerArtista.validarDados, controllerArtista.criar);
-router.get('/', controllerArtista.listarTodos)
-router.get('/:id', controllerArtista.buscarId, controllerArtista.buscar)
+router.post('/', validarToken, controllerArtista.validarDados, controllerArtista.criar);
+router.get('/', validarToken, controllerArtista.listarTodos)
+router.get('/:id', validarToken, controllerArtista.buscarId, controllerArtista.buscar)
 router.put(
   '/:id',
+  validarToken,
   controllerArtista.buscarId,
   controllerArtista.validarDados,
   controllerArtista.atualizar
 )
 router.delete(
   '/:id',
+  validarToken,
   controllerArtista.buscarId,
   controllerArtista.remover
 )

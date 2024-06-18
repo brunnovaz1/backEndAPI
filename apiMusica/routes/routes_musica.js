38cb1822ba4661d5b56830d950b4f3f2daf7dd81
Laryss M <laryss.moraes@gmail.com>
2024-06-18 17:51:28 -0300

    corrige path da api

--- apiMusica/routes/routes_musica.js
+++ apiMusica/routes/routes_musica.js
@@ -6,14 +6,15 @@ const controllerMusica = require('../controllers/controller_musica');
 
 const router = express.Router();
 
-router.post('/', controllerMusica.validarDados, controllerMusica.criar);
+router.post('/', /*validarToken,*/ controllerMusica.validarDados, controllerMusica.criar);
 
 router.get('/',validarToken, controllerMusica.obterTodos);
 
-router.get('/:id', controllerMusica.buscarPeloId, controllerMusica.obter);
+router.get('/:id', /*validarToken,*/ controllerMusica.buscarPeloId, controllerMusica.obter);
 
 router.put(
     "/:id",
+    /*validarToken,*/
     controllerMusica.buscarPeloId,
     controllerMusica.validarDados, 
     controllerMusica.atualizar

aff18dbb1eebfc5f8f4842f268166b0580549400
sasaestudo2018 <sasaestudo2018@gmail.com>
2024-06-18 17:29:22 -0300

    Close #17: Finaliza implementação do model

--- colecoes/models/colecoesModel.js
+++ colecoes/models/colecoesModel.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
 
-
-
-
 const musicaSchema = new mongoose.Schema({
   titulo: { type: String, required: [true, 'O título da música é obrigatório.'] },
   artista: { type: String, required: [true, 'O artista da música é obrigatório.'] }
@@ -14,5 +11,6 @@ const colecaoSchema = new mongoose.Schema({
   autor: { type: String, trim: true, required: [true, 'O campo "autor" é obrigatório.'] }, 
   musicas: { type: [musicaSchema], trim: true, required: [true, 'É obrigatório que haja ao menos uma música incluída na coleção.'] }
 });
+
 
 module.exports = mongoose.model('Colecao', colecaoSchema);

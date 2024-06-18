5930bae06556092f7477480519c8043170e29d6e
brunnovaz1 <brunnovaz@gmail.com>
2024-06-18 12:06:45 -0300

    atualizacao auth rota post (close #5.1)

--- api/routes/router_artista.js
+++ api/routes/router_artista.js
@@ -4,7 +4,6 @@ const controllerArtista = require('../controllers/controller_artista');
 
 const router = express.Router();
 
-
 router.post('/', validarToken, controllerArtista.validarDados, controllerArtista.criar);
 router.get('/', validarToken, controllerArtista.listarTodos)
 router.get('/:id', validarToken, controllerArtista.buscarId, controllerArtista.buscar)
